edd15c9759b679d903d92bd0dc2c68c3fc652ee7
Jason Finney <finney.jason.218@gmail.com>
2018-09-10 11:07:40 -0500

    initialized II

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var PORT = 9000;
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./routing/htmlRouting")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
